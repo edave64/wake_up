760e9e5fd85e3eb2e0ad0afa58524c40b478e951
edave64 <github5815@xcvbnm.org>
2025-05-04 21:20:19 +0200

    feat: rgb circle

--- wake_up.ino
+++ wake_up.ino
@@ -1,16 +1,28 @@
 #include <RTClib.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 150
+#define DATA_PIN 6
+CRGB leds[NUM_LEDS];
 
 RTC_DS3231 rtc;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   
-  if (! rtc.begin()) {
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
 
+  leds[0] = 0xFF0000;
+  leds[1] = 0xFFFF00;
+  leds[2] = 0x00FF00;
+  leds[3] = 0x00FFFF;
+  leds[4] = 0x0000FF;
+  leds[5] = 0xFF00FF;
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
@@ -56,4 +68,11 @@ void loop() {
     String line = Serial.readStringUntil("\n");
     processInput(line);
   }
+  CRGB tmp = leds[0];
+  for (int i = 1; i < NUM_LEDS; ++i) {
+    leds[i - 1] = leds[i];
+  }
+  leds[NUM_LEDS-1] = tmp;
+  FastLED.show();
+  delay(30);
 }
