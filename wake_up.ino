a4b2c7d306df68e170c64bcb326576d9350f7335
edave64 <github5815@xcvbnm.org>
2025-05-04 21:36:44 +0200

    feat: LED power tester

--- wake_up.ino
+++ wake_up.ino
@@ -16,13 +16,6 @@ void setup() {
     Serial.println("Couldn't find RTC");
     while (1);
   }
-
-  leds[0] = 0xFF0000;
-  leds[1] = 0xFFFF00;
-  leds[2] = 0x00FF00;
-  leds[3] = 0x00FFFF;
-  leds[4] = 0x0000FF;
-  leds[5] = 0xFF00FF;
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
@@ -63,16 +56,22 @@ void processInput(String input) {
   }
 }
 
+int i = 0;
+
 void loop() {
   if (Serial.available()) {
     String line = Serial.readStringUntil("\n");
     processInput(line);
   }
-  CRGB tmp = leds[0];
-  for (int i = 1; i < NUM_LEDS; ++i) {
-    leds[i - 1] = leds[i];
+  leds[i] = CRGB::White;
+  if (i < NUM_LEDS - 1) {
+    ++i;
+  } else {
+    for (i = 0; i < NUM_LEDS; ++i) {
+      leds[i] = CRGB::Black;
+    }
+    i = 0;
   }
-  leds[NUM_LEDS-1] = tmp;
   FastLED.show();
-  delay(30);
+  delay(100);
 }
