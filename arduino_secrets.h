2d2d09e9a59fe5acab1231a51a6e0f49cd4db220
edave64 <github5815@xcvbnm.org>
2025-05-30 19:22:01 +0200

    Add google credentials support

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,2 +1,5 @@
 #define SECRET_SSID "" //network name
 #define SECRET_PASS "" //network password
+#define SECRET_GOOGLE_CLIENT_SECRET ""
+#define SECRET_GOOGLE_CLIENT_ID ""
+#define SERCET_GOOGLE_REFRESH ""
